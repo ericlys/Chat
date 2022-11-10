6da7061aa435ef93764429fe3d2d0a81479ca154
Ericlys Moreira <ericlysm@gmail.com>
2022-11-10 06:48:26 -0300

    feat: configuring socket in application

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
 import express from "express";
+import path from "path";
+import {createServer} from "http";
+import { Server } from "socket.io"
+
 const app = express();
+
+const server = createServer(app);
+
+app.use(express.static(path.join(__dirname, "..", "public")))
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("Socket", socket.id)
+})
 
 app.get("/", (request, response) => {
   return response.json({
@@ -7,4 +21,4 @@ app.get("/", (request, response) => {
   })
 })
 
-app.listen(3000, () => console.log("server is running on 3000"))
+server.listen(3000, () => console.log("server is running on 3000"))
