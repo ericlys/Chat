e92754d56dd41396542ee95a0d8bff21b66e9c68
Ericlys Moreira <ericlysm@gmail.com>
2022-11-11 06:32:51 -0300

    feat: create user service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,36 @@
+import { injectable } from "tsyringe";
+import { User } from "../schemas/User"
+
+
+interface CreateUserDTO {
+  email: string,
+  socket_id: string,
+  avatar: string,
+  name: string,
+}
+
+@injectable()
+class CreateUseService {
+  async execute({email, socket_id, avatar, name}: CreateUserDTO){
+    const userAlreadyExists = await User.findOne({
+      email
+    }).exec();
+
+    if(userAlreadyExists) {
+      const user = await User.findOneAndUpdate({
+        _id: userAlreadyExists._id
+      },
+      {
+        $set: { socket_id, avatar, name }
+      });
+      return user;
+    }else {
+      const user = await User.create({
+        email, socket_id, avatar, name
+      });
+      return user;
+    }
+  }
+}
+
+export { CreateUseService }
