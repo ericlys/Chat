4af52fd0addc2fb14307731fc710c2c420d0eac8
Ericlys Moreira <ericlysm@gmail.com>
2022-11-12 12:34:54 -0300

    feat: communication between users

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,6 @@
 const socket = io("http://localhost:3000");
 
-// let idChatRoom = "";
+  let idChatRoom = "";
 
   function onLoad() {
     const urlParams = new URLSearchParams(window.location.search);
@@ -40,99 +40,21 @@ const socket = io("http://localhost:3000");
     });
   }
 
+  document.getElementById("users_list").addEventListener("click", (e) => {
 
-//   socket.on("message", (data) => {
-//     if (data.message.roomId === idChatRoom) {
-//       addMessage(data);
-//     }
-//   });
-
-//   socket.on("notification", (data) => {
-//     if (data.roomId !== idChatRoom) {
-//       const user = document.getElementById(`user_${data.from._id}`);
-
-//       user.insertAdjacentHTML(
-//         "afterbegin",
-//         ` 
-//         <div class="notification"></div>
-//       `
-//       );
-//     }
-//   });
-// }
-
-// function addMessage(data) {
-//   const divMessageUser = document.getElementById("message_user");
-
-//   divMessageUser.innerHTML += ` 
-//   <span class="user_name user_name_date">
-//       <img
-//         class="img_user"
-//         src=${data.user.avatar}
-//       />
-//       <strong> ${data.user.name} &nbsp; </strong>
-//       <span>  ${dayjs(data.message.created_at).format(
-//         "DD/MM/YYYY HH:mm"
-//       )} </span></span
-//     >
-//     <div class="messages">
-//       <span class="chat_message"> ${data.message.text}</span>
-//     </div>
-//   `;
-// }
+  if (e.target && e.target.matches("li.user_name_list")) {
+    const idUser = e.target.getAttribute("idUser");
+    console.log(idUser)
 
 
-// document.getElementById("users_list").addEventListener("click", (e) => {
-//   const inputMessage = document.getElementById("user_message");
-//   inputMessage.classList.remove("hidden");
+    socket.emit("start_chat", { idUser }, (data) => {
+      console.log(data);
+      idChatRoom = data.idChatRoom
+    });
 
-//   document
-//     .querySelectorAll("li.user_name_list")
-//     .forEach((item) => item.classList.remove("user_in_focus"));
-
-//   document.getElementById("message_user").innerHTML = "";
-//   if (e.target && e.target.matches("li.user_name_list")) {
-//     const idUser = e.target.getAttribute("idUser");
-
-//     e.target.classList.add("user_in_focus");
-
-//     const notification = document.querySelector(
-//       `#user_${idUser} .notification`
-//     );
-//     if (notification) {
-//       notification.remove();
-//     }
-
-//     socket.emit("start_chat", { idUser }, (response) => {
-//       idChatRoom = response.room.idChatRoom;
-
-//       response.messages.forEach((message) => {
-//         const data = {
-//           message,
-//           user: message.to,
-//         };
-
-//         addMessage(data);
-//       });
-//     });
-//   }
-// });
-
-// document.getElementById("user_message").addEventListener("keypress", (e) => {
-//   if (e.key === "Enter") {
-//     const message = e.target.value;
-
-//     e.target.value = "";
-
-//     const data = {
-//       message,
-//       idChatRoom,
-//     };
-
-//     socket.emit("message", data);
-//   }
-// });
-
+  
+  }
+});
 
 function addUser(user) {
   const usersList = document.getElementById("users_list");
