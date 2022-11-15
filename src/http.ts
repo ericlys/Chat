c35c02140fc72f4389a066a074195a6ee5f50408
Ericlys Moreira <ericlysm@gmail.com>
2022-11-15 14:05:37 -0300

    fix: removing console

--- src/http.ts
+++ src/http.ts
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, "..", "public")))
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  console.log("Socket", socket.id)
+  // console.log("Socket", socket.id)
 })
 
 app.get("/", (request, response) => {
